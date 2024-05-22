9f4c73312e3e8462dcbd3bd23ef70d5b84516957
Rodrigo-T-Matioli <57012803+Rodrigo-T-Matioli@users.noreply.github.com>
2024-05-22 00:03:20 -0300

    Module Vercel

--- src/app.module.ts
+++ src/app.module.ts
@@ -42,7 +42,7 @@ import { UsuariosModule } from './usuarios/usuarios.module';
       password: process.env.DATABASE_PASSWORD,
       entities: [join(__dirname, '**/*entity.{ts,js}')],
       database: process.env.DATABASE_NAME,
-      synchronize: true,
+      synchronize: false,
       namingStrategy: new SnakeNamingStrategy(),
     }),
     ClientesModule,
