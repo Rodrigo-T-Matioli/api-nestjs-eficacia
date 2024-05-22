6b7a4e508e8038af206feb3b630521c19cc38b5a
Rodrigo-T-Matioli <57012803+Rodrigo-T-Matioli@users.noreply.github.com>
2024-05-21 23:48:15 -0300

    Ajuste banco vercel

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
 import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
 import { NestFactory, Reflector } from '@nestjs/core';
+import { config } from 'dotenv';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
+  config();
   await app.listen(3000);
 }
 bootstrap();
