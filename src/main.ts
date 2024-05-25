b96b3064a67ffe046cd4b3565cff6279027518cf
Rodrigo-T-Matioli <57012803+Rodrigo-T-Matioli@users.noreply.github.com>
2024-05-25 01:01:08 -0300

    Cors

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { config } from 'dotenv';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: false });
+  const app = await NestFactory.create(AppModule, { cors: true });
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
   config();
