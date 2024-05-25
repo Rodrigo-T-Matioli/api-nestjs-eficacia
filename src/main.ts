db218f069be105de965400ebd6a79a962bcaf016
Rodrigo-T-Matioli <57012803+Rodrigo-T-Matioli@users.noreply.github.com>
2024-05-25 00:37:52 -0300

    Cors ajuste

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,14 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
+  app.enableCors({
+    origin: 'https://eficacia-angular.vercel.app',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS', 'HEAD'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    exposedHeaders: ['Content-Length', 'X-Total-Count'],
+    credentials: true,
+    maxAge: 86400,
+  });
   config();
   await app.listen(3000);
 }
