6c9523552fcdb80a25629946746115ca7f429725
Rodrigo-T-Matioli <57012803+Rodrigo-T-Matioli@users.noreply.github.com>
2024-05-25 00:56:59 -0300

    Cors Vercel

--- src/main.ts
+++ src/main.ts
@@ -4,17 +4,9 @@ import { config } from 'dotenv';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule, { cors: false });
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
-  app.enableCors({
-    origin: 'https://eficacia-angular.vercel.app',
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS', 'HEAD'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    exposedHeaders: ['Content-Length', 'X-Total-Count'],
-    credentials: true,
-    maxAge: 86400,
-  });
   config();
   await app.listen(3000);
 }
